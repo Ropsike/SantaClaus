f300c79fb4bda875a5844cfd47c059753d9d9500
Opilane TTHK <opilane@tthk.ee>
2021-12-14 11:48:32 +0200

    santa

---
+++ santa/santa/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace santa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.treetop = new System.Windows.Forms.PictureBox();
+            this.Santa = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.treebottom = new System.Windows.Forms.PictureBox();
+            this.ScoreText = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.treetop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Santa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.treebottom)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::santa.Properties.Resources.hut;
+            this.pictureBox1.Location = new System.Drawing.Point(613, 372);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(172, 138);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::santa.Properties.Resources.moon;
+            this.pictureBox2.Location = new System.Drawing.Point(229, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 91);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::santa.Properties.Resources.snowflake;
+            this.pictureBox3.Location = new System.Drawing.Point(734, 284);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 82);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // treetop
+            // 
+            this.treetop.Image = global::santa.Properties.Resources.tree;
+            this.treetop.Location = new System.Drawing.Point(518, 3);
+            this.treetop.Name = "treetop";
+            this.treetop.Size = new System.Drawing.Size(100, 132);
+            this.treetop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.treetop.TabIndex = 3;
+            this.treetop.TabStop = false;
+            // 
+            // Santa
+            // 
+            this.Santa.Image = global::santa.Properties.Resources.sleigh;
+            this.Santa.Location = new System.Drawing.Point(24, 222);
+            this.Santa.Name = "Santa";
+            this.Santa.Size = new System.Drawing.Size(200, 88);
+            this.Santa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Santa.TabIndex = 4;
+            this.Santa.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.Image = global::santa.Properties.Resources.ground;
+            this.ground.Location = new System.Drawing.Point(2, 505);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(846, 118);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 5;
+            this.ground.TabStop = false;
+            // 
+            // treebottom
+            // 
+            this.treebottom.Image = global::santa.Properties.Resources.tree;
+            this.treebottom.Location = new System.Drawing.Point(399, 345);
+            this.treebottom.Name = "treebottom";
+            this.treebottom.Size = new System.Drawing.Size(100, 184);
+            this.treebottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.treebottom.TabIndex = 6;
+            this.treebottom.TabStop = false;
+            // 
+            // ScoreText
+            // 
+            this.ScoreText.AutoSize = true;
+            this.ScoreText.Font = new System.Drawing.Font("SimSun", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreText.Location = new System.Drawing.Point(12, 12);
+            this.ScoreText.Name = "ScoreText";
+            this.ScoreText.Size = new System.Drawing.Size(151, 33);
+            this.ScoreText.TabIndex = 9;
+            this.ScoreText.Text = "Score: 0";
+            this.ScoreText.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(846, 602);
+            this.Controls.Add(this.ScoreText);
+            this.Controls.Add(this.treebottom);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.Santa);
+            this.Controls.Add(this.treetop);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.treetop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Santa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.treebottom)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox treetop;
+        private System.Windows.Forms.PictureBox Santa;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox treebottom;
+        private System.Windows.Forms.Label ScoreText;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
---
+++ santa/santa/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace santa
+{
+    public partial class Form1 : Form
+    {
+        int treeSpeed = 8;
+        int gravity = 15;
+        int score = 0;
+        
+        
+        
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Santa.Top += gravity;
+            treebottom.Left -= treeSpeed;
+            treetop.Left -= treeSpeed;
+            ScoreText.Text = score.ToString();
+
+            if(treebottom.Left < -50)
+            {
+                treebottom.Left = 800;
+                score++;
+            }
+            if(treetop.Left < -80)
+            {
+                treetop.Left = 950;
+                score++;
+            }
+
+            if(Santa.Bounds.IntersectsWith(treebottom.Bounds) ||
+              Santa.Bounds.IntersectsWith(treetop.Bounds) ||
+              Santa.Bounds.IntersectsWith(ground.Bounds)
+              )
+            {
+                endGame();
+            }
+
+        }
+
+        private void gamekeyisdown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+                gravity = -15;
+            }
+
+
+
+        }
+
+        private void gamekeyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 15;
+            }
+        }
+    
+    
+     private void endGame()
+     {
+      GameTimer.Stop();
+      ScoreText.Text +="Game Over!";
+     }
+    
+    
+    
+    }
+
+
+
+}
